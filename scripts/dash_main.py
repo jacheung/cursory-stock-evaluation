6829222948a785cbb8e4ac70a75f6e32b3ef2239
Jonathan Cheung <jacheung6@gmail.com>
2021-08-13 11:27:30 -0700

    json data transform for data file share

--- scripts/dash_main.py
+++ scripts/dash_main.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 from lib import evaluation_tools as tools
+import pandas as pd
 
 app = dash.Dash(__name__)
 
@@ -21,18 +22,29 @@ app.layout = html.Div([
         value='2017-01-01'
     ),
     dcc.Graph(id='stock-ROI-scatter'),
-    dcc.Graph(id='stock-daily-histogram')
+    dcc.Graph(id='stock-daily-histogram'),
+    dcc.Store(id='intermediate-df')
 ])
 
 
 @app.callback(
-    Output('stock-ROI-scatter', 'figure'),
+    Output('intermediate-df', 'data'),
     Input('stock-tickers', 'value'),
     Input('date', 'value')
 )
-def update_figure(stock_tickers, date):
+def build_dataframe_json(stock_tickers, date):
     stock_tickers = tools.clean_string(stock_tickers)
     df = tools.build_figure_df(stock_tickers, start_date=date)
+    json_df = df.reset_index(drop=True).to_json()
+    return json_df
+
+
+@app.callback(
+    Output('stock-ROI-scatter', 'figure'),
+    Input('intermediate-df', 'data')
+)
+def update_scatter(json_df):
+    df = pd.read_json(json_df)
     scatter_df, frequency = tools.downsample(df)
     fig = px.scatter(scatter_df, x="Date", y="% ROI", color="ticker",
                      title='Stock ' + frequency + ' % ROI')
@@ -43,12 +55,10 @@ def update_figure(stock_tickers, date):
 
 @app.callback(
     Output('stock-daily-histogram', 'figure'),
-    Input('stock-tickers', 'value'),
-    Input('date', 'value')
+    Input('intermediate-df', 'data')
 )
-def update_histogram(stock_tickers, date):
-    stock_tickers = tools.clean_string(stock_tickers)
-    df = tools.build_figure_df(stock_tickers, start_date=date)
+def update_histogram(json_df):
+    df = pd.read_json(json_df)
     hist_bins = df['Percent change'].quantile([.01, .99]).round(2).values
     fig = px.histogram(df, x='Percent change', color='ticker', nbins=151, range_x=hist_bins.tolist(),
                        histnorm='probability', marginal="rug")
--- scripts/test_main.py
+++ scripts/test_main.py
@@ -1,16 +1,20 @@
 import plotly.express as px
 from lib import evaluation_tools as tools
+import pandas as pd
 
-stock_tickers = 'VOO, QQQ'
+stock_tickers = 'VOO, MDIJX'
 date = []
 stock_tickers = tools.clean_string(stock_tickers)
 df = tools.build_figure_df(stock_tickers, start_date=date)
+df = pd.read_json(df.reset_index(drop=True).to_json())
 scatter_df, frequency = tools.downsample(df)
-# fig = px.scatter(scatter_df, x="Date", y="% ROI", color="ticker",
-#                  title='Stock ' + frequency + ' % ROI')
-# fig.update_layout(hovermode="x unified")
+fig = px.scatter(scatter_df, x="Date", y="% ROI", color="ticker",
+                 title='Stock ' + frequency + ' % ROI')
+fig.update_traces(mode='lines+markers')
+fig.update_layout(hovermode="x unified")
 
-hist_bins = df['Percent change'].quantile([.01, .99]).round(2).values
-fig = px.histogram(df, x='Percent change', color='ticker', nbins=151, range_x=hist_bins.tolist(),
-                   histnorm='probability', marginal="rug")
+# hist_bins = df['Percent change'].quantile([.01, .99]).round(2).values
+# fig = px.histogram(df, x='Percent change', color='ticker', nbins=151, range_x=hist_bins.tolist(),
+#                    histnorm='probability', marginal="rug")
+
 fig.show()
